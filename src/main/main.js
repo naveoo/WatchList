7c7eefa806837fbb94c0e0ad88cc045686650a76
naveo <naa.veoos@gmail.com>
2025-05-12 11:58:59 +0200

    💰

--- src/main/main.js
+++ src/main/main.js
@@ -10,9 +10,33 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const dbPath = path.join(__dirname, '../data.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
-        console.error('Erreur lors de la connexion à la base de données:', err.message);
+        console.error('Erreur lors de la connexion à la base de données', err);
     } else {
-        console.log('Base de données connectée à :', dbPath);
+        console.log('Connecté à la base de données SQLite.');
+        db.run(`
+            CREATE TABLE IF NOT EXISTS favorites (
+                id INTEGER PRIMARY KEY,
+                movie_id INTEGER UNIQUE,
+                title TEXT,
+                poster TEXT,
+                release_date TEXT
+            )
+        `, (err) => {
+            if (err) console.error("Erreur lors de la création de la table favorites:", err);
+        });
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS watchlist (
+                id INTEGER PRIMARY KEY,
+                movie_id INTEGER UNIQUE,
+                title TEXT,
+                poster TEXT,
+                release_date TEXT,
+                duration INTEGER
+            )
+        `, (err) => {
+            if (err) console.error("Erreur lors de la création de la table watchlist:", err);
+        });
     }
 });
 
