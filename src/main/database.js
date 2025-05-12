b13f3978895a7aa717e3a80b37fb804c3fe57c8a
naveo <naa.veoos@gmail.com>
2025-05-12 11:53:46 +0200

    ✨

--- src/main/database.js
+++ src/main/database.js
@@ -7,6 +7,30 @@ const db = new sqlite3.Database(dbPath, (err) => {
         console.error('Erreur lors de la connexion à la base de données', err);
     } else {
         console.log('Connecté à la base de données SQLite.');
+        db.run(`
+            CREATE TABLE IF NOT EXISTS favorites (
+                id INTEGER PRIMARY KEY,
+                movie_id INTEGER UNIQUE,
+                title TEXT,
+                poster TEXT,
+                release_date TEXT
+            )
+        `, (err) => {
+            if (err) console.error("Erreur lors de la création de la table favorites:", err);
+        });
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS watchlist (
+                id INTEGER PRIMARY KEY,
+                movie_id INTEGER UNIQUE,
+                title TEXT,
+                poster TEXT,
+                release_date TEXT,
+                duration INTEGER
+            )
+        `, (err) => {
+            if (err) console.error("Erreur lors de la création de la table watchlist:", err);
+        });
     }
 });
 
